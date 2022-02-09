b9272a596628ff9ffe1caf7b5ff7bda69e0c9517
Force4760 <gspteixeira@gmail.com>
2022-02-09 16:50:14 +0000

    Initial Commit.

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/Force4760/pipes
+
+require (
+	
+	github.com/spf13/cobra v0.0.3
+	
+)
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	
+	"github.com/Force4760/pipes/cmd"
+)
+
+func main() {
+
+    
+    cmd.Execute()
+	
+}
