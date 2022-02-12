0fbfb3d0c1ee7288c9321f9a264d35817d9813f9
Force4760 <gspteixeira@gmail.com>
2022-02-12 09:54:53 +0000

    FunctionFIle string representation

--- src/ast/program.go
+++ src/ast/program.go
@@ -3,6 +3,10 @@ package ast
 // Datastructure conteining the operations of an individual file
 type FunctionFile []Expression
 
+func (f *FunctionFile) String() string {
+	return toStr(*f)
+}
+
 // Datastructure that holds all different files
 type Program struct {
 	Files map[string]Expression
