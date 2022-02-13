671487abc02da0666162986bacdef9e03b654efd
Force4760 <gspteixeira@gmail.com>
2022-02-13 10:34:13 +0000

    Edit comments

--- src/ast/program.go
+++ src/ast/program.go
@@ -5,10 +5,12 @@ import "encoding/json"
 // Datastructure conteining the operations of an individual file
 type FunctionFile []Expression
 
+// Convert FunctionFile to string
 func (f *FunctionFile) String() string {
 	return toStr(*f)
 }
 
+// Convert FunctionFile to json
 func (f *FunctionFile) ToJson() string {
 	js, err := json.MarshalIndent(f, "", "  ")
 
